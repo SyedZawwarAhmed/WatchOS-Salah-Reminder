6cc668add3f7fc0cbd408536441574b010cbd5cb
ZawwarAhmedIOMechs <zawwar.ahmed@iomechs.com>
2024-05-06 18:49:49 +0500

    Initial Commit

---
+++ Salah Reminder Watch App/Salah_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salah_ReminderApp.swift
+//  Salah Reminder Watch App
+//
+//  Created by Zawwar Ahmed on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Salah_Reminder_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
